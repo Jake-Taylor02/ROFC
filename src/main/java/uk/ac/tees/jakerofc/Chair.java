3b5869c295be5456a44a69d8082a7286c08238ab
jake <jake@DESKTOP-TVER3H1.home>
2022-02-16 20:07:18 +0000

    Initial

---
+++ src/main/java/uk/ac/tees/jakerofc/Chair.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uk.ac.tees.jakerofc;
+
+import java.io.File;
+import javax.swing.ImageIcon;
+
+
+/**
+ *
+ * @author b1086175
+ */
+public class Chair extends Item {
+    private boolean armrests;
+
+    public Chair(String ID, WoodType wood, int quantity, boolean armrests) {
+        super(ID, wood, quantity);
+        this.armrests = armrests;
+    }
+    
+    @Override
+    protected void calculatePrice(){
+        int units = 1625;
+        
+        if (armrests) units += 250;
+        
+        this.itemPrice = this.wood.getPrice() * units;
+    }
+
+    @Override
+    protected void setImage() {
+        image = new ImageIcon("chair3.png");
+    }
+
+    @Override
+    public void newItem() {
+        new NewItemFrame();
+    }
+
+    @Override
+    public String getSummary() {
+        return "";
+    }
+
+    public boolean isArmrests() {
+        return armrests;
+    }
+
+    public void setArmrests(boolean armrests) {
+        this.armrests = armrests;
+        calculatePrice();
+    }
+    
+    
+    
+    
+    
+}
