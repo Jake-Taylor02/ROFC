372d7719346df4337773c2a9e2a8b3b0896d4959
jake <jake@DESKTOP-TVER3H1.home>
2022-02-16 20:08:06 +0000

    Initial

---
+++ src/main/java/uk/ac/tees/jakerofc/TableBase.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package uk.ac.tees.jakerofc;
+
+/**
+ *
+ * @author b1086175
+ */
+public enum TableBase {
+    WOODEN(4500),
+    CHROME(3500);
+    
+    private final int price;
+
+    private TableBase(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+    
+    
+}
