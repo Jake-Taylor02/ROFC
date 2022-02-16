265867c0bc05d2cfd71d006147a4dfcb8c026b4e
jake <jake@DESKTOP-TVER3H1.home>
2022-02-16 20:07:09 +0000

    Initial

---
+++ src/main/java/uk/ac/tees/jakerofc/Item.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uk.ac.tees.jakerofc;
+
+import java.io.File;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author b1086175
+ */
+public abstract class Item {
+    private String ID;
+    protected WoodType wood;
+    protected int itemPrice;
+    private int quantity;
+    protected ImageIcon image;
+
+    public Item(String ID, WoodType wood, int quantity) {
+        this.ID = ID;
+        this.wood = wood;
+        this.quantity = quantity;
+        
+        setImage();
+    }
+    
+    
+    protected abstract void calculatePrice();
+    
+    protected abstract void setImage();
+    
+    public static ImageIcon defaultImage() {
+        return new ImageIcon("no_image2.jpg");
+    }
+    
+    public ImageIcon getImage() {
+        return image;
+    }
+    
+    public abstract void newItem();
+    
+    public abstract String getSummary();
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public WoodType getWood() {
+        return wood;
+    }
+
+    public void setWood(WoodType wood) {
+        this.wood = wood;
+        calculatePrice();
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+    
+    
+}
