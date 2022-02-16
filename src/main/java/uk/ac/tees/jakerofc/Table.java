7cb1aa872de871f4b24da2f1ae4d5cd1d7ba5b75
jake <jake@DESKTOP-TVER3H1.home>
2022-02-16 20:07:40 +0000

    Initial

---
+++ src/main/java/uk/ac/tees/jakerofc/Table.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uk.ac.tees.jakerofc;
+
+import java.io.File;
+import javax.swing.ImageIcon;
+
+
+/**
+ *
+ * @author b1086175
+ */
+public class Table extends Item{
+    private TableBase base;
+    private int diameter;
+
+    public Table(String ID, WoodType wood, int quantity, TableBase base, int diameter) {
+        super(ID, wood, quantity);
+        this.base = base;
+        this.diameter = diameter;
+    }
+    
+    @Override
+    protected void calculatePrice() {
+        int units = diameter * diameter;
+        
+        this.itemPrice = (units * wood.getPrice()) + base.getPrice();
+    }
+    
+    @Override
+    protected void setImage() {
+        image = new ImageIcon("table2.jpg");
+    }
+
+    @Override
+    public void newItem() {
+        new NewItemFrame();
+    }
+
+    @Override
+    public String getSummary() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public TableBase getBase() {
+        return base;
+    }
+
+    public void setBase(TableBase base) {
+        this.base = base;
+        calculatePrice();
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+        calculatePrice();
+    }
+    
+    
+}
