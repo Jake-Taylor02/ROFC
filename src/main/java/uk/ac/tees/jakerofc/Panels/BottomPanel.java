c30ea35acbb42dd7615dfab49e2d9a665aab1dae
jake <jake@DESKTOP-TVER3H1>
2022-02-17 23:42:40 +0000

    Refactored Bottom Panel

---
+++ src/main/java/uk/ac/tees/jakerofc/Panels/BottomPanel.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uk.ac.tees.jakerofc.Panels;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import uk.ac.tees.jakerofc.ROFCapp;
+
+/**
+ *
+ * @author jake
+ */
+public class BottomPanel extends Box implements ActionListener {
+    JButton jlTotal;
+    JButton jbSummary;
+    
+    public BottomPanel() {
+        super(BoxLayout.X_AXIS);
+        
+        jlTotal = new JButton("Total");
+        jlTotal.addActionListener(this);
+        
+        jbSummary = new JButton("Show Summary");
+        jbSummary.addActionListener(this);
+        
+        this.add(jlTotal);
+        this.add(Box.createHorizontalGlue());
+        this.add(jbSummary);
+        
+        
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == jlTotal) {
+            JOptionPane.showMessageDialog(this, "Total: " + ROFCapp.itemArr.calcTotal());
+        } else if (e.getSource() == jbSummary) {
+            JOptionPane.showMessageDialog(this, ROFCapp.itemArr.orderSummary());
+            System.out.println(ROFCapp.itemArr.orderSummary());
+        }
+    }
+    
+}
