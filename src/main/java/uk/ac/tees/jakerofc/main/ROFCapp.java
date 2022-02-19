a1ae9774176c813d6f23e62cf8f2134a77bb1bd1
jake <jake@DESKTOP-TVER3H1>
2022-02-19 22:09:43 +0000

    refactored package structure

---
+++ src/main/java/uk/ac/tees/jakerofc/main/ROFCapp.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uk.ac.tees.jakerofc.main;
+
+import uk.ac.tees.jakerofc.main.TopPanel;
+import uk.ac.tees.jakerofc.main.BottomPanel;
+import uk.ac.tees.jakerofc.main.LeftPanel;
+import uk.ac.tees.jakerofc.main.CenterPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+import javax.swing.*;
+import uk.ac.tees.jakerofc.Chair;
+import uk.ac.tees.jakerofc.Desk;
+import uk.ac.tees.jakerofc.newitem.NewItemFrame;
+import uk.ac.tees.jakerofc.Order;
+import uk.ac.tees.jakerofc.Table;
+import uk.ac.tees.jakerofc.TableBase;
+import uk.ac.tees.jakerofc.WoodType;
+
+/**
+ *  * set image, use static in subclasses for image?
+ * @author b1086175
+ */
+public class ROFCapp extends JFrame {
+    public static Order itemArr = new Order();
+    public static ROFCapp myFrame;// neccesary?
+    
+    public static void main(String[] args) {
+        
+        // test items
+        itemArr.addItem(new Chair("001", WoodType.OAK, 1, true));
+        itemArr.addItem(new Desk("002", WoodType.WALNUT, 1, 100, 75, 3));
+        itemArr.addItem(new Desk("003", WoodType.WALNUT, 1, 100, 75, 3));
+        itemArr.addItem(new Chair("004", WoodType.OAK, 2, true));
+        itemArr.addItem(new Table("005", WoodType.OAK, 1, TableBase.WOODEN, 60));
+        
+        myFrame = new ROFCapp();
+        
+        new NewItemFrame();
+    }
+
+    public ROFCapp() throws HeadlessException {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("ROFC");
+        this.setLayout(new BorderLayout(5, 5));
+        
+        // define top panel and add it to frame
+        JPanel jpTop = new TopPanel();
+        this.add(jpTop, BorderLayout.NORTH);
+
+        // define left panel and add it to frame
+        JPanel jpLeft = new LeftPanel();
+        this.add(jpLeft, BorderLayout.WEST);
+        
+        
+        // Define center grid and add it to frame
+        JPanel jpCenter = new CenterPanel(itemArr);
+        JScrollPane jsp = new JScrollPane(jpCenter);
+        jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        this.add(jsp, BorderLayout.CENTER);
+        
+        
+        
+        // Define bottom Panel
+        BottomPanel jpBottom = new BottomPanel();
+        this.add(jpBottom, BorderLayout.SOUTH);
+        
+        this.pack();
+        this.setVisible(true);
+    }
+    
+   
+    
+    
+}
