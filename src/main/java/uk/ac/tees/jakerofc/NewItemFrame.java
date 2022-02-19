7bea506ebc466ff55af7188fd6a5c617cdc75902
jake <jake@DESKTOP-TVER3H1>
2022-02-19 21:32:45 +0000

    NewItemFrame Main Panel refactored

--- src/main/java/uk/ac/tees/jakerofc/NewItemFrame.java
+++ src/main/java/uk/ac/tees/jakerofc/NewItemFrame.java
@@ -28,95 +28,108 @@ import javax.swing.SwingConstants;
  * @author Jake
  */
 public class NewItemFrame extends JFrame {
-    protected JTextField txtidNum;
-    protected JComboBox jcbWoodType;
-    protected JSpinner spQuantity;
     
     protected JPanel jpFooter;
     
-    SpringLayout spLayout;
-    JPanel jpForm;
+    protected SpringLayout spLayout;
+    protected JPanel jpForm;
     
     public NewItemFrame() throws HeadlessException {
         this.setTitle("Add New <item>");// show furniture type?
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         
         this.setLayout(new BorderLayout());
-        
-        // ID Number
-        JLabel jlIdNum = new JLabel("ID Number:", SwingConstants.RIGHT);
-        jlIdNum.setPreferredSize(new Dimension(100, 20));
-        
-        txtidNum = new JTextField();
-        txtidNum.setPreferredSize(new Dimension(120, 20));
-        
-        // Wood type
-        JLabel jlWoodType = new JLabel("Type of Wood:", SwingConstants.RIGHT);
-        jlWoodType.setPreferredSize(new Dimension(100, 20));
-        
-        jcbWoodType = new JComboBox(WoodType.values());
-        jcbWoodType.setPreferredSize(new Dimension(120, 20));
-        
-        // Quantity
-        JLabel jlQuantity = new JLabel("Quantity:", SwingConstants.RIGHT);
-        jlQuantity.setPreferredSize(new Dimension(100, 20));
-        
-        SpinnerNumberModel qModel = new SpinnerNumberModel(
-            1,
-            1,
-            20,
-            1
-        );
-        spQuantity = new JSpinner(qModel);
-        spQuantity.setPreferredSize(new Dimension(120, 20));
-        
-        // Set up spring layout and add components
-        spLayout = new SpringLayout();
-        jpForm = new JPanel();
-        jpForm.setLayout(spLayout);
-        
-        jpForm.add(jlIdNum);
-        jpForm.add(txtidNum);
-        
-        jpForm.add(jlWoodType);
-        jpForm.add(jcbWoodType);
-        
-        jpForm.add(jlQuantity);
-        jpForm.add(spQuantity);
-        
-        
-        //Place constraints on ID Number Label and Field
-        spLayout.putConstraint(SpringLayout.WEST, jlIdNum, 5, SpringLayout.WEST, jpForm);
-        spLayout.putConstraint(SpringLayout.NORTH, jlIdNum, 5, SpringLayout.NORTH, jpForm);
-        spLayout.putConstraint(SpringLayout.WEST, txtidNum, 5, SpringLayout.EAST, jlIdNum);
-        spLayout.putConstraint(SpringLayout.NORTH, txtidNum, 5, SpringLayout.NORTH, jpForm);
-        
-        //Place constraints on Wood Type
-        spLayout.putConstraint(SpringLayout.WEST, jlWoodType, 5, SpringLayout.WEST, jpForm);
-        spLayout.putConstraint(SpringLayout.NORTH, jlWoodType, 5, SpringLayout.SOUTH, txtidNum);
-        spLayout.putConstraint(SpringLayout.WEST, jcbWoodType, 5, SpringLayout.EAST, jlWoodType);
-        spLayout.putConstraint(SpringLayout.NORTH, jcbWoodType, 5, SpringLayout.SOUTH, jlIdNum);
-        
-        //Place constraints on Quantity
-        spLayout.putConstraint(SpringLayout.WEST, jlQuantity, 5, SpringLayout.WEST, jpForm);
-        spLayout.putConstraint(SpringLayout.NORTH, jlQuantity, 5, SpringLayout.SOUTH, jlWoodType);
-        spLayout.putConstraint(SpringLayout.WEST, spQuantity, 5, SpringLayout.EAST, jlQuantity);
-        spLayout.putConstraint(SpringLayout.NORTH, spQuantity, 5, SpringLayout.SOUTH, jcbWoodType);
-        
+    
+        initCenter();
         
         // Add Footer buttons - need to add listeners
         initFooter();
         
-        //this.setLayout(new BorderLayout());
-        this.add(jpForm, BorderLayout.CENTER);// add in subclass?
+        
         this.setSize(400, 400);
         this.setVisible(true);// should be in subclass
     }
     
+    protected void initCenter() {
+        class CenterPanel extends JPanel implements ActionListener {
+            protected JTextField txtidNum;
+            protected JComboBox jcbWoodType;
+            protected JSpinner spQuantity;
+    
+            public CenterPanel() {
+                // ID Number
+                JLabel jlIdNum = new JLabel("ID Number:", SwingConstants.RIGHT);
+                jlIdNum.setPreferredSize(new Dimension(100, 20));
+
+                txtidNum = new JTextField();
+                txtidNum.setPreferredSize(new Dimension(120, 20));
+                
+                // Wood type
+                JLabel jlWoodType = new JLabel("Type of Wood:", SwingConstants.RIGHT);
+                jlWoodType.setPreferredSize(new Dimension(100, 20));
+
+                jcbWoodType = new JComboBox(WoodType.values());
+                jcbWoodType.setPreferredSize(new Dimension(120, 20));
+
+                // Quantity
+                JLabel jlQuantity = new JLabel("Quantity:", SwingConstants.RIGHT);
+                jlQuantity.setPreferredSize(new Dimension(100, 20));
+
+                SpinnerNumberModel qModel = new SpinnerNumberModel(
+                    1,
+                    1,
+                    20,
+                    1
+                );
+                spQuantity = new JSpinner(qModel);
+                spQuantity.setPreferredSize(new Dimension(120, 20));
+                
+                // Set up spring layout and add components
+                spLayout = new SpringLayout();
+                this.setLayout(spLayout);
+
+                this.add(jlIdNum);
+                this.add(txtidNum);
+
+                this.add(jlWoodType);
+                this.add(jcbWoodType);
+
+                this.add(jlQuantity);
+                this.add(spQuantity);
+                
+                //Place constraints on ID Number Label and Field
+                spLayout.putConstraint(SpringLayout.WEST, jlIdNum, 5, SpringLayout.WEST, this);
+                spLayout.putConstraint(SpringLayout.NORTH, jlIdNum, 5, SpringLayout.NORTH, this);
+                spLayout.putConstraint(SpringLayout.WEST, txtidNum, 5, SpringLayout.EAST, jlIdNum);
+                spLayout.putConstraint(SpringLayout.NORTH, txtidNum, 5, SpringLayout.NORTH, this);
+
+                //Place constraints on Wood Type
+                spLayout.putConstraint(SpringLayout.WEST, jlWoodType, 5, SpringLayout.WEST, this);
+                spLayout.putConstraint(SpringLayout.NORTH, jlWoodType, 5, SpringLayout.SOUTH, txtidNum);
+                spLayout.putConstraint(SpringLayout.WEST, jcbWoodType, 5, SpringLayout.EAST, jlWoodType);
+                spLayout.putConstraint(SpringLayout.NORTH, jcbWoodType, 5, SpringLayout.SOUTH, jlIdNum);
+
+                //Place constraints on Quantity
+                spLayout.putConstraint(SpringLayout.WEST, jlQuantity, 5, SpringLayout.WEST, this);
+                spLayout.putConstraint(SpringLayout.NORTH, jlQuantity, 5, SpringLayout.SOUTH, jlWoodType);
+                spLayout.putConstraint(SpringLayout.WEST, spQuantity, 5, SpringLayout.EAST, jlQuantity);
+                spLayout.putConstraint(SpringLayout.NORTH, spQuantity, 5, SpringLayout.SOUTH, jcbWoodType);
+            }
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+            
+        }
+        jpForm = new CenterPanel();
+        this.add(jpForm, BorderLayout.CENTER);
+    }
+    
     private void initFooter() {
         class FooterPanel extends JPanel implements ActionListener {
-            JButton jbCancel;
-            JButton jbSave;
+            private JButton jbCancel;
+            private JButton jbSave;
 
             public FooterPanel() {
                 jbCancel = new JButton("Cancel");
@@ -129,8 +142,6 @@ public class NewItemFrame extends JFrame {
                 
                 this.add(jbCancel);
                 this.add(jbSave);
-                
-                
             }
             
             @Override
