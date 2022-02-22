a93077286502574cda614888679cf6e5abb5a6f0
jake <jake@DESKTOP-TVER3H1>
2022-02-22 13:53:28 +0000

    observer

--- src/main/java/uk/ac/tees/jakerofc/main/ROFCapp.java
+++ src/main/java/uk/ac/tees/jakerofc/main/ROFCapp.java
@@ -6,6 +6,8 @@ package uk.ac.tees.jakerofc.main;
 
 import java.awt.BorderLayout;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 import uk.ac.tees.jakerofc.Chair;
 import uk.ac.tees.jakerofc.Desk;
@@ -20,23 +22,20 @@ import uk.ac.tees.jakerofc.WoodType;
  * @author b1086175
  */
 public class ROFCapp extends JFrame {
-    public static Order itemArr = new Order();
+    private Order itemArr = new Order();
     
     public static void main(String[] args) {
-        
-        // test items
+        new ROFCapp();
+    }
+
+    public ROFCapp() throws HeadlessException {
+        // Test items
         itemArr.addItem(new Chair("001", WoodType.OAK, 1, true));
         itemArr.addItem(new Desk("002", WoodType.WALNUT, 1, 100, 75, 3));
         itemArr.addItem(new Desk("003", WoodType.WALNUT, 1, 100, 75, 3));
         itemArr.addItem(new Chair("004", WoodType.OAK, 2, true));
         itemArr.addItem(new Table("005", WoodType.OAK, 1, TableBase.WOODEN, 60));
-        
-        new ROFCapp();
-        
-        
-    }
-
-    public ROFCapp() throws HeadlessException {
+        //
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("ROFC");
         this.setLayout(new BorderLayout(5, 5));
@@ -59,14 +58,47 @@ public class ROFCapp extends JFrame {
         
         
         // Define bottom Panel
-        BottomPanel jpBottom = new BottomPanel();
-        this.add(jpBottom, BorderLayout.SOUTH);
+        initBottom();
         
         this.pack();
         this.setVisible(true);
     }
     
-   
+    private void initBottom() {
+        class BottomPanel extends Box implements ActionListener {
+            JButton jlTotal;
+            JButton jbSummary;
+            
+            public BottomPanel() {
+                super(BoxLayout.X_AXIS);
+                
+                jlTotal = new JButton("Total");
+                jlTotal.addActionListener(this);
+        
+                jbSummary = new JButton("Show Summary");
+                jbSummary.addActionListener(this);
+        
+                this.add(jlTotal);
+                this.add(Box.createHorizontalGlue());
+                this.add(jbSummary);
+            }
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == jlTotal) {
+                    JOptionPane.showMessageDialog(this, String.format("Total: £%.2f",
+                    (double) itemArr.calcTotal() / 100));
+            
+                } else if (e.getSource() == jbSummary) {
+                    JOptionPane.showMessageDialog(this, itemArr.orderSummary());
+                    System.out.println(itemArr.orderSummary());
+                }
+            }
+            
+        }
+        BottomPanel jpBottom = new BottomPanel();
+        this.add(jpBottom, BorderLayout.SOUTH);
+    }
     
     
 }
