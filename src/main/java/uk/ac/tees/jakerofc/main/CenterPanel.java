f9b7572e330c67bcbfabb3ea3314e0fcdc55585a
b1086175 <b1086175@linux36.scedt.tees.ac.uk>
2022-02-25 10:48:52 +0000

    init2

--- src/main/java/uk/ac/tees/jakerofc/main/CenterPanel.java
+++ src/main/java/uk/ac/tees/jakerofc/main/CenterPanel.java
@@ -6,6 +6,8 @@ package uk.ac.tees.jakerofc.main;
 
 
 import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,25 +43,50 @@ public class CenterPanel extends JPanel {
     
     private void init(){
         this.removeAll();
-        this.setLayout(new GridLayout(3, 3, 5, 5));
+        /*this.setLayout(new GridLayout(3, 3, 5, 5));
         
         labels = new JLabel[9];
         for (int i = 0; i < 9; i++) {
+        
+        // create the new label
+        labels[i] = new JLabel();
+        labels[i].setBorder(BorderFactory.createLineBorder(Color.BLUE));
+        
+        // if the grid actually contains an item, set the image
+        if (i < itemArr.size()) {
+        labels[i].setIcon(itemArr.get(i).getImage());
+        } else {
+        labels[i].setIcon(Item.defaultImage());
+        }
+        this.add(labels[i]);
+        
+        }
+        this.updateUI();*/
+        
+        this.setLayout(new GridBagLayout());
+        int x = 0, y = 0;
+        for (Item it : itemArr) {
+            GridBagConstraints gbPos = new GridBagConstraints();
+            gbPos.gridx = x;
+            gbPos.gridy = y;
             
-            // create the new label
-            labels[i] = new JLabel();
-            labels[i].setBorder(BorderFactory.createLineBorder(Color.BLUE));
+            JLabel jlnew = new JLabel();
+            jlnew.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+            jlnew.setIcon(it.getImage());
+            this.add(jlnew, gbPos);
             
-            // if the grid actually contains an item, set the image
-            if (i < itemArr.size()) {
-                labels[i].setIcon(itemArr.get(i).getImage());
-            } else {
-                labels[i].setIcon(Item.defaultImage());
+            
+            x++;
+            if (x >= 3) {
+                x = 0;
+                y++;
             }
-            this.add(labels[i]);
             
         }
-        this.updateUI();
+    }
+    
+    private void init2() {
+        
     }
     
     public void updateItems() {
