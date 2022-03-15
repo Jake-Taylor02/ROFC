8a31f6a7d340eacc6c94c47d63ff6baadbb04623
jake <jake@DESKTOP-TVER3H1>
2022-03-15 19:17:57 +0000

    main panel

---
+++ src/main/java/uk/ac/tees/b1086175/ROFCApp/main/MainPanel.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uk.ac.tees.b1086175.ROFCApp.main;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import uk.ac.tees.b1086175.ROFCApp.Order;
+
+/**
+ *
+ * @author jake
+ */
+public class MainPanel extends JPanel{
+    private final Order itemArr = Order.getInstance();
+    
+    private CenterPanel jpCenter;
+    private TopPanel jpTop;
+    private JPanel jpLeft;
+    private BottomPanel jpBottom;
+
+    public MainPanel() {
+        init();
+    }
+    
+    private void init() {
+        this.setLayout(new BorderLayout(5, 5));
+        
+        jpTop = new TopPanel();
+        this.add(jpTop, BorderLayout.NORTH);
+        
+        jpCenter = new CenterPanel();
+        JScrollPane jsp = new JScrollPane(jpCenter);
+        jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        this.add(jsp, BorderLayout.CENTER);
+        
+        jpTop.setChangeItemListener(jpCenter);// !! Change tto list?
+        
+        jpLeft = new LeftPanel(jpCenter);
+        this.add(jpLeft, BorderLayout.WEST);
+        
+        jpBottom = new BottomPanel();
+        this.add(jpBottom, BorderLayout.SOUTH);
+    }
+            
+    class BottomPanel extends Box implements ActionListener {
+
+        JButton jlTotal;
+        JButton jbSummary;
+
+        public BottomPanel() {
+            super(BoxLayout.X_AXIS);
+
+            init();
+        }
+        
+        private void init() {
+            jlTotal = new JButton("Total");
+            jlTotal.addActionListener(this);
+
+            jbSummary = new JButton("Show Summary");
+            jbSummary.addActionListener(this);
+
+            this.add(jlTotal);
+            this.add(Box.createHorizontalGlue());
+            this.add(jbSummary);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == jlTotal) {
+
+                JOptionPane.showMessageDialog(this, String.format("Total: £%.2f",
+                    (double) itemArr.calcTotal() / 100));
+
+            } else if (e.getSource() == jbSummary) {
+                JOptionPane.showMessageDialog(this, new SummaryPanel(),
+                    "Order Summary", JOptionPane.INFORMATION_MESSAGE);
+                
+                System.out.println(itemArr.orderSummary());
+            }
+        }
+    } 
+    
+}
--- src/main/java/uk/ac/tees/b1086175/ROFCApp/main/ROFCapp.java
+++ src/main/java/uk/ac/tees/b1086175/ROFCApp/main/ROFCapp.java
@@ -19,89 +19,33 @@ import javax.swing.*;
  * @author b1086175 | Jake Taylor
  */
 public class ROFCapp extends JFrame {
-    private Order itemArr = Order.getInstance();
-    
-    CenterPanel jpCenter;
+    private MainPanel mainPanel;
+    private static final Order itemArr = Order.getInstance();
     
     public static void main(String[] args) {
-        new ROFCapp();
-    }
-
-    public ROFCapp() throws HeadlessException {
         // Test items
         itemArr.addItem(new Chair("001", WoodType.OAK, 1, true));
         itemArr.addItem(new Desk("002", WoodType.WALNUT, 1, 100, 75, 3));
         itemArr.addItem(new Desk("003", WoodType.WALNUT, 1, 100, 75, 3));
         itemArr.addItem(new Chair("004", WoodType.OAK, 2, true));
         itemArr.addItem(new Table("005", WoodType.OAK, 1, TableBase.WOODEN, 60));
+        
+        new ROFCapp();
+    }
 
+    public ROFCapp() throws HeadlessException {
+        init();
+    }
+    
+    private void init() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("ROFC");
-        this.setLayout(new BorderLayout(5, 5));
         
-        // define top panel and add it to frame
-        TopPanel jpTop = new TopPanel();
-        this.add(jpTop, BorderLayout.NORTH);
-        
-        // Define center grid and add it to frame
-        jpCenter = new CenterPanel();
-        JScrollPane jsp = new JScrollPane(jpCenter);
-        jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        this.add(jsp, BorderLayout.CENTER);
-        
-        jpTop.setChangeItemListener(jpCenter);
-        
-        // define left panel and add it to frame
-        JPanel jpLeft = new LeftPanel(jpCenter);
-        this.add(jpLeft, BorderLayout.WEST);
-        
-        
-        
-        
-        
-        // Define bottom Panel
-        initBottom();
+        mainPanel = new MainPanel();
+        add(mainPanel);
         
         this.pack();
         this.setVisible(true);
-    }
-    
-    private void initBottom() {
-        class BottomPanel extends Box implements ActionListener {
-            JButton jlTotal;
-            JButton jbSummary;
-            
-            public BottomPanel() {
-                super(BoxLayout.X_AXIS);
-                
-                jlTotal = new JButton("Total");
-                jlTotal.addActionListener(this);
-        
-                jbSummary = new JButton("Show Summary");
-                jbSummary.addActionListener(this);
-        
-                this.add(jlTotal);
-                this.add(Box.createHorizontalGlue());
-                this.add(jbSummary);
-            }
-            
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (e.getSource() == jlTotal) {
-                    
-                    JOptionPane.showMessageDialog(this, String.format("Total: £%.2f",
-                    (double) itemArr.calcTotal() / 100));
-            
-                } else if (e.getSource() == jbSummary) {
-                    JOptionPane.showMessageDialog(this, new SummaryPanel(), "Order Summary", JOptionPane.INFORMATION_MESSAGE);
-                    //JOptionPane.showMessageDialog(this, itemArr.orderSummary());
-                    System.out.println(itemArr.orderSummary());
-                }
-            }
-            
-        }
-        BottomPanel jpBottom = new BottomPanel();
-        this.add(jpBottom, BorderLayout.SOUTH);
     }
 
    
