ef65784dcec7ffcfff475bc29fb2c486f8d2138a
b1086175 <b1086175@linux36.scedt.tees.ac.uk>
2022-03-17 10:55:12 +0000

    over

---
+++ src/main/java/uk/ac/tees/b1086175/ROFCApp/view/Itemview.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package uk.ac.tees.b1086175.ROFCApp.view;
+
+/**
+ *
+ * @author b1086175
+ */
+public interface Itemview {
+    
+}
